08e04adaddf0bf72a9ee5f8fb0370b83bdc6de6f
Vladyslav Reva <averdalv@gmail.com>
2026-08-02 23:18:32 +0300

    fix: consume executor credential in called environment

--- scripts/render_repository_workflows.py
+++ scripts/render_repository_workflows.py
@@ -168,7 +168,6 @@ jobs:
       execution_record_app_id: ${{{{ vars.EXECUTION_RECORD_APP_ID }}}}
     secrets:
       EXECUTION_RECORD_APP_PRIVATE_KEY: ${{{{ secrets.EXECUTION_RECORD_APP_PRIVATE_KEY }}}}
-      EXECUTOR_CREDENTIAL: ${{{{ secrets.EXECUTOR_CREDENTIAL }}}}
 """
 
 
